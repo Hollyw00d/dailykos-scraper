6357732d18c3855cd0fdfa74fcd8c92559248927
Matt Jennings <matt@mattjennings.net>
2019-09-26 19:20:06 -0700

    added variables assigned to node packages

---
+++ server.js
@@ -0,0 +1,7 @@
+var axios = require('axios');
+var cheerio = require('cheerio');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
