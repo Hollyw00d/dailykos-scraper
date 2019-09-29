495383c54509ba4474b3da4e2b0fa1544cba686c
Matt Jennings <matt@mattjennings.net>
2019-09-29 12:47:02 -0700

    added scrape success and failure messags

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
 
-    //
-    $.ajax({
-        url: '/scrape',
-        method: 'GET'
-    })
-    .then(function(data) {
-        
-    });
+    function scrapeDailyKos() {
+        var scrapeSuccess  = $('#scrape-success');
+        var scrapeFail = $('#scrape-fail');
+
+        $.ajax({
+            url: '/scrape',
+            method: 'GET'
+        })
+        // If scrape successful
+        // show success msg and hide fail msg
+        .done(function(data) {
+            scrapeFail.addClass('d-none');
+            scrapeSuccess.removeClass('d-none');
+        })
+        // If scrape failure
+        // show fail msg and hide success msg
+        .fail(function(data) {
+            scrapeFail.removeClass('d-none');
+            scrapeSuccess.addClass('d-none');
+        });
+    }
+
+    scrapeDailyKos();
     
 });
