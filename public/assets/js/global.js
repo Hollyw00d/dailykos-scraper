26d27bdcdaaba997dccbf3ee4d3cdf6175f9ca39
Matt Jennings <matt@mattjennings.net>
2019-09-29 13:15:07 -0700

    middle of trying to add save article link

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
         var scrapeSuccess  = $('#scrape-success');
         var scrapeFail = $('#scrape-fail');
 
+        var articleTable = $('#article-table > tbody');
+
         $.ajax({
             url: '/scrape',
             method: 'GET'
@@ -13,12 +15,16 @@ $(document).ready(function() {
         .done(function(data) {
             scrapeFail.addClass('d-none');
             scrapeSuccess.removeClass('d-none');
+            data.map(function(article, index) {
+                articleTable.append('<tr><td><h3 class="h4"><a href="' + article.link + '" target="_blank">' + article.headline + '</a></h3>' + '<p><a href="#" class="save-article text-success" data-id="' + index + '">Save Article</a></p>' + '</td></tr>');
+            });
         })
         // If scrape failure
         // show fail msg and hide success msg
         .fail(function(data) {
             scrapeFail.removeClass('d-none');
             scrapeSuccess.addClass('d-none');
+            articleTable.empty();
         });
     }
 
