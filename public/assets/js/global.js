e25870e1c51e299c52bc951884399357c48ff4f2
Matt Jennings <matt@mattjennings.net>
2019-09-29 12:27:07 -0700

    added padding to top and bottom of parent container

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
+    //
     $.ajax({
         url: '/scrape',
         method: 'GET'
     })
     .then(function(data) {
-        console.log(data);
+        
     });
     
 });
